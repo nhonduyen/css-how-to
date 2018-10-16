a828735ccf46cd0b07a55d94fd6fc1f3b8e271fb
duyen <nhonduyen@gmail.com>
2018-10-16 11:03:42 +0700

    First commit

---
+++ js/menu/tabheader.js
@@ -0,0 +1,19 @@
+function openCity(cityName, elmnt, color) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(cityName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+} 
+document.getElementById("defaultOpen").click();
---
+++ js/menu/tabs.js
@@ -0,0 +1,14 @@
+document.getElementById("defaultOpen").click();
+function openCity(evt, cityName) {
+    console.log(evt);
+    const tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    const tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += "active";
+}
