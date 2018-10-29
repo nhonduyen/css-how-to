9a3f928ceb0aa6765ddf66bcf6ce09db457a65db
duyen <nhonduyen@gmail.com>
2018-10-29 11:56:56 +0700

    Add table section

--- js/forms/multistepform.js
+++ js/forms/multistepform.js
@@ -1,47 +1,50 @@
 let currentTab = 0;
-
-let ckb = document.getElementById("mycheck");
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
+prevBtn.addEventListener("click", function () { nextPrev(-1); }, false);
+nextBtn.addEventListener("click", function () { nextPrev(1); }, false);
+showTab(currentTab);
 
 function showTab(n) {
     let tab = document.getElementsByClassName("tab");
     tab[n].style.display = "block";
     if (n == 0) {
-        document.getElementById("prevBtn").style.display = "none";
+        prevBtn.style.display = "none";
     }
     else {
-        document.getElementById("prevBtn").style.display = "inline";
+        prevBtn.style.display = "inline";
     }
     if (n == tab.length - 1) {
-        document.getElementById("nextBtn").innerHTML = "Submit";
+        nextBtn.innerHTML = "Submit";
     }
     else {
-        document.getElementById("nextBtn").innerHTML = "Next";
+        nextBtn.innerHTML = "Next";
     }
     fixStepIndicator(n);
 }
 function nextPrev(n) {
     let tabs = document.getElementsByClassName("tab");
-    if (n == 1 || !validateForm())
+    if (n == 0 || !validateForm())
         return false;
     tabs[currentTab].style.display = "none";
     currentTab += n;
     if (currentTab == tabs.length - 1) {
         document.getElementById("regForm").submit();
-        return;
+        return false;
     }
     showTab(currentTab);
 }
 function fixStepIndicator(n) {
     let step = document.getElementsByClassName("step");
     for (let i = 0; i < step.length; i++) {
-        step[i].className = step[i].className.replace(" active", "");
+        step[i].className = step[i].className.replace("active", "");
     }
     step[n].className += " active";
 }
 function validateForm() {
     let valid = true;
     let tabs = document.getElementsByClassName("tab");
-    let inputs = document.getElementsByTagName("input");
+    let inputs = tabs[currentTab].getElementsByTagName("input");
     for (let i = 0; i < inputs.length; i++) {
         if (!inputs[i].value) {
             valid = false;
